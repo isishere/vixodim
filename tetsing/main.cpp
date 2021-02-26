d6724c2b0a37e07a85c75eb672dd071f77aff715
Ivan <70635146+themilchenko@users.noreply.github.com>
2021-02-26 23:50:40 +0300

    Update main.cpp

--- tetsing/main.cpp
+++ tetsing/main.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 #include <chrono>
+
+int plus(double& a, double& b)
+{
+	return a + b;
+}
+
+int main()
+{
+	double a = 3.14;
+	double b = 0.86;
+
+	auto begin = std::chrono::system_clock::now();
+	std::cout << plus(a, b) << std::endl;
+	auto end = std::chrono::system_clock::now();
+
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+	return 0;
+}
