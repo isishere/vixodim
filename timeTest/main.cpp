de19fab630c3232367a7abd8afd795c63d0aa6f4
ilya <71101318+isishere@users.noreply.github.com>
2021-04-17 00:15:09 +0300

    Create main.cpp

---
+++ timeTest/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <chrono>
+
+int getSum(double& a, double& b)
+{
+	return a + b;
+}
+
+int main()
+{
+	double a = 3.14;
+	double b = 0.86;
+
+	auto begin = std::chrono::system_clock::now();
+	std::cout << getSum(a, b) << std::endl;
+	auto end = std::chrono::system_clock::now();
+
+	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+  
+	return 0;
+}
